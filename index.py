0a812f8a6a478b09362b062e3061dc5effbb4ea7
adityarev <aditya.vritalia@gmail.com>
2018-11-17 12:23:13 +0700

    Complete index

--- index.py
+++ index.py
@@ -2,6 +2,34 @@ import bottle
 import pymongo
 import guestbookDAO
 
-@bottle.route('/')
-def guestbook_index():
 
+class Index:
+    def __init__(self):
+        self.connection_string = 'mongodb://localhost'
+
+    # This is the default route, our index page. Here we need to read the documents from MongoDB
+    @bottle.route('/')
+    def guestbook_index(self):
+        persons = guestbook.find_persons()
+        return bottle.template('index', dict(persons=persons))
+
+    # We will post new entries to this route so we can insert them into MongoDB
+    @bottle.route('/newguest', method='POST')
+    def insert_newguest(self):
+        name = bottle.request.forms.get('name')
+        email = bottle.request.forms.get('email')
+        guestbook.insert_person(name, email)
+        bottle.redirect('/')
+
+    def run(self):
+        connection = pymongo.MongoClient(self.connection_string)
+        database = connection.persons
+        guestbook = guestbookDAO.GuestbookDAO(database)
+
+        bottle.debug(True)
+        bottle.run(host='localhost', port=8082)
+
+
+if __name__ == '__main__':
+    index = Index()
+    index.run()
